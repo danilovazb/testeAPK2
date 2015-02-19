049ebf85b0067d176460e6c552da3219e2f468d4
Danilo Vaz <danilovazb@gmail.com>
2015-02-19 14:37:48 -0200

    lol

---
+++ www/index.php
@@ -0,0 +1,27 @@
+<script src="js/jquery-1.11.1.min.js" type="text/javascript" ></script>
+
+<form method="post" id="infoForm">
+    <input type="text" name="first_name" id="first_name" value="" placeholder="First Name"  />
+    <input type="text" name="last_name" id="last_name" value="" placeholder="Last Name"  />   
+    <input type="text" name="email" id="email" value="" placeholder="Email"  />
+    <button type="submit">Submit</button> 
+</form>
+
+<script>
+$('#infoForm').submit(function() {
+ 
+    var postTo = 'http://192.168.2.122/AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+ 
+    $.post(postTo,({first_name: $('[name=first_name]').val(), last_name: $('[name=last_name]').val(), email: $('[name=email]').val()}),
+    function(data) {
+        alert(data);
+        if(data != "") {
+            // do something
+        } else {
+            // couldn't connect
+        }
+        },'json');
+    return false;
+ 
+});
+</script>
